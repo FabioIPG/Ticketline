a144da41eaa813ed9ce6c628569c04ee674d8f2a
Fabio Abreu <fabiofiqueli@hotmail.com>
2022-06-01 00:12:45 +0100

    created function to convert an Event into ContentValues

--- app/src/main/java/pt/ipg/ticketline/Evento.kt
+++ app/src/main/java/pt/ipg/ticketline/Evento.kt
@@ -1,13 +1,24 @@
 package pt.ipg.ticketline
 
+import android.content.ContentValues
 import java.util.*
 
 data class Evento(
         var id: Long,
         var nome_evento: String,
-        var data: Date,
+        var data: String,
         var artistaId: Long,
         var localId: Long
 
 ) {
+    fun toContentValues() : ContentValues {
+        val valores = ContentValues()
+
+        valores.put(TabelaBDEventos.CAMPO_NOME,nome_evento )
+        valores.put(TabelaBDEventos.CAMPO_DATA, data)
+        valores.put(TabelaBDEventos.CAMPO_ARTISTA_ID, artistaId)
+        valores.put(TabelaBDEventos.CAMPO_LOCAL_ID, localId)
+
+        return valores
+    }
 }
