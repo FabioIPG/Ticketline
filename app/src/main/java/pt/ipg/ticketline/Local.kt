a5b66c51c72f88a5968d272deefc09b568510c05
Fabio Abreu <fabiofiqueli@hotmail.com>
2022-06-01 00:11:49 +0100

    created function to convert a Location into ContentValues

--- app/src/main/java/pt/ipg/ticketline/Local.kt
+++ app/src/main/java/pt/ipg/ticketline/Local.kt
@@ -1,4 +1,28 @@
 package pt.ipg.ticketline
 
-data class Local(var id: Long, var nome: String) {
+import android.content.ContentValues
+import android.provider.BaseColumns
+
+data class Local(
+        var id: Long,
+        var nome_local: String,
+        var localizacao: String,
+        var endereco: String,
+        var propriatario: String,
+        var ano_inauguracao: Int,
+        var tipo: String,
+        var capacidade: Int
+        ) {
+    fun toContentValues() : ContentValues {
+        val valores = ContentValues()
+        valores.put(TabelaBDLocais.CAMPO_NOME, nome_local)
+        valores.put(TabelaBDLocais.CAMPO_LOCALIZACAO, localizacao)
+        valores.put(TabelaBDLocais.CAMPO_ENDRECO, endereco)
+        valores.put(TabelaBDLocais.CAMPO_PROPRIATARIO, propriatario)
+        valores.put(TabelaBDLocais.CAMPO_ANO_INAUGURACAO, ano_inauguracao)
+        valores.put(TabelaBDLocais.CAMPO_TIPO, tipo)
+        valores.put(TabelaBDLocais.CAMPO_CAPACIDADE, capacidade)
+
+        return valores
+    }
 }
